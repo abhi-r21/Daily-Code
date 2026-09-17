16c9a8e504394b2d49af453a06e1e8e5bb26d18d
abhi-r21 <connerbrain14@gmail.com>
2026-09-17 16:04:28 +0530

    Add: JS Question 12 Solution

--- JS100Questions/12.js
+++ JS100Questions/12.js
@@ -1,3 +1,11 @@
 //Check whether a number is Even or Odd.
 //Input: let number = 17;
 // Expected Output : Odd 
+
+let number = 17;
+
+if (number % 2 == 0) {
+  console.log("The number is even");
+} else {
+  console.log("The number is odd");
+}
