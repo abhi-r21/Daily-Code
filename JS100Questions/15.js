d272ebcd440be9fc0b83d1d04c37ef7cfe7e8875
abhi-r21 <connerbrain14@gmail.com>
2026-09-17 16:23:05 +0530

    ADD: JS Question 15 solution

--- JS100Questions/15.js
+++ JS100Questions/15.js
@@ -1,2 +1,10 @@
 //check whether a number is divisible by both 3 and 5.
 // let number = 30;
+
+let number = 30 ;
+
+if (number % 3 == 0 && number % 5 == 0) {
+  console.log("The Number is divisible by both");
+} else {
+  console.log("The Number is not Divisible by both");
+}
