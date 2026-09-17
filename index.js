fe3a68e6922f78e08ac0a038598ba0c576a49d40
abhi-r21 <connerbrain14@gmail.com>
2026-09-17 10:17:05 +0530

    Add: JS Question 01

---
+++ index.js
@@ -0,0 +1,13 @@
+//JavaScript 1-100 practice questions challenge.
+//Topic: Variables, data types, & Basic operations.
+
+// 1. Create variables for your name, age and whether your a student. Print all these
+// Output: Abhiram, 22 and true. 
+
+let personName = "Abhiram";
+let age = 22;
+let isStudent = true;
+
+console.log(personName);
+console.log(age);
+console.log(isStudent);
