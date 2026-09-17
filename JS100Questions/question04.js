1dd91205a2ab74db46ca191755d16584b6dfada2
abhi-r21 <connerbrain14@gmail.com>
2026-09-17 10:59:13 +0530

    Add: JS Question 04 Solution

--- JS100Questions/question04.js
+++ JS100Questions/question04.js
@@ -1,3 +1,7 @@
 /* 4. Create a variable containing your full name: "My name is Abhiram" using a string concatenation.
 Output: My name is Abhiram */
 
+let firstName = "Abhiram";
+let lastName = "Bojja";
+
+console.log(`My name is ${firstName} ${lastName}`);
