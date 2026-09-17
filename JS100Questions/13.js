bd6df3f6a91ee4422a13f3d022a73c556cfe34aa
abhi-r21 <connerbrain14@gmail.com>
2026-09-17 16:07:59 +0530

    ADD: JS Question 13 solution

--- JS100Questions/13.js
+++ JS100Questions/13.js
@@ -1,3 +1,10 @@
 // Check whether a person is eligible to vote
 //let age = 20;
 
+let age = 20;
+
+if (age >= 18) {
+  console.log("The person is eligible to vote");
+} else {
+  console.log("The person is not eligible to vote");
+}
