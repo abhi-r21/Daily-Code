3f25f7f082c3a78561ea9555cbab54121b6cb5c4
abhi-r21 <connerbrain14@gmail.com>
2026-09-17 10:30:53 +0530

    Add: JS Question 02

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //JavaScript 1-100 practice questions challenge.
+//East-Medium-Hard
+
 //Topic: Variables, data types, & Basic operations.
 
-// 1. Create variables for your name, age and whether your a student. Print all these
-// Output: Abhiram, 22 and true. 
+/* 1. Create variables for your name, age and whether your a student. Print all these
+ Output: Abhiram, 22 and true. */
 
 let personName = "Abhiram";
 let age = 22;
@@ -11,3 +13,28 @@ let isStudent = true;
 console.log(personName);
 console.log(age);
 console.log(isStudent);
+
+/*2. Create two numbers: (let a= 15, b =7). print their sum, difference, multiplication and division.
+Output: 22, 8, 105, 2.142857142857143 */
+
+let a = 15;
+let b = 7;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+/* 3. What will this output ?
+  let x = 10;
+  let y = "20",
+  console.log(x+y);
+  console.log(x-y);
+expected output : (1020, -10) */ 
+
+/* 4. Create a variable containing your full name: "My name is Abhiram" using a string concatenation.
+Output: My name is Abhiram */
+
+/* 5. Predict the output: let a = 10, let b = 20, a = b, b = 30 
+console.log(a), console.log(b)
+Output: 20, 30  */
