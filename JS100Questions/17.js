321cda38bd271f97f45c052385f7074ad7c9161b
abhi-r21 <connerbrain14@gmail.com>
2026-09-18 09:56:36 +0530

    ADD: JS Question 17 solution

--- JS100Questions/17.js
+++ JS100Questions/17.js
@@ -1,2 +1,14 @@
 //Find the largest among the three numbers
 // let a = 25, let b = 75, let c = 50;
+
+let a = 25;
+let b = 75;
+let c = 50;
+
+if (a > b && a > c) {
+  console.log("A is larger number");
+} else if (b > a && b > c ) {
+  console.log("B is larger number");
+} else if (c > a && c > b){
+  console.log("C is larger number");
+};
