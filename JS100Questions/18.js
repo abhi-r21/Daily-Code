e1bd6b8ff45159956f8cb51d11f35c8527ceb271
abhi-r21 <connerbrain14@gmail.com>
2026-09-18 10:01:40 +0530

    ADD: JS Question 18.js soution

--- JS100Questions/18.js
+++ JS100Questions/18.js
@@ -1,2 +1,12 @@
 //write a program that checks : age > = 18 and hasLicense = true;
 //input : let age = 22 and let hasLicense = true;
+
+let age = 12;
+let hasLicense = true;
+let hasNoLicense = false;
+
+if (age >= 18) {
+  console.log(hasLicense);
+}else {
+  console.log(hasNoLicense);
+}
