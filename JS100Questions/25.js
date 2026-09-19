f8af28b4fe469a7ac15013656b0eea072611f00c
abhi-r21 <connerbrain14@gmail.com>
2026-09-19 10:34:37 +0530

    Add: JS Question 25.js Solution

--- JS100Questions/25.js
+++ JS100Questions/25.js
@@ -1,1 +1,12 @@
 //Count how many numbers between 1 and 100 are divisible by 7.
+
+let num = 7;
+let count = 0;
+
+for(let i = 1; i <= 100; i++) {
+  if (i % num == 0) {
+  count = count + 1;
+  }
+}
+
+console.log(count);
