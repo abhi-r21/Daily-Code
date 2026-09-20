b6513fbb1c78e1baf50deb88182bca827a6c3995
abhi-r21 <connerbrain14@gmail.com>
2026-09-20 10:14:04 +0530

    Add: JS Question 26.js Solution

--- JS100Questions/26.js
+++ JS100Questions/26.js
@@ -1,2 +1,13 @@
 //Reverse this number using a loop:
 // let number = 12345;
+
+let number = 12345;
+let reversed = 0;
+
+while(number > 0) {
+  let digit = number % 10;
+  reversed = reversed * 10 + digit;
+  number = Math.floor(number/10);
+}
+
+console.log(reversed);
